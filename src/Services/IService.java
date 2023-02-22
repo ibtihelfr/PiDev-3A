5506756f0d691b3f92529d3c0eae9e57f3e87586
Brahim711 <brahim.hamdouni@esprit.tn>
2023-02-22 09:59:59 +0100

    Interface Produit

---
+++ src/Services/IService.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Services;
+
+import java.sql.SQLException;
+import java.util.List;
+
+/**
+ *
+ * @author Brahim
+ */
+public interface IService<P> {
+
+    void ajouter(P p) ;
+
+    void update(P p) ;
+
+    void supprime(P p) ;
+
+    List<P> readAll() ;
+
+    P findById(int IdProduit) ;
+}
---
+++ src/Utils/DataSource.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Utils;
+
+import java.sql.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Brahim
+ */
+public class DataSource {
+
+    private static DataSource data;
+    private Connection con;
+    private String url = "jdbc:mysql://localhost:3306/tunmix";
+    private String login = "root";
+    private String pwd = "";
+
+    private DataSource() {
+
+        try {
+            con = DriverManager.getConnection(url, login, pwd);
+            System.out.println("Connexion établie");
+        } catch (SQLException ex) {
+            System.out.println(ex);
+        }
+    }
+
+    public Connection getConnection() {
+        return con;
+    }
+
+    public static DataSource getInstance() {
+        if (data == null) {
+            data = new DataSource();
+        }
+        return data;
+    }
+}
